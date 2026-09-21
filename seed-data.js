ce37fb4bebe7b05c05370c312343e4193097cc18
transparencia-mendonca <miguel.angelogarcia@gmail.com>
2026-09-21 00:48:15 -0300

    Add files via upload

---
+++ seed-data.js
@@ -0,0 +1,2 @@
+window.SEED_DATA={_bundleVersion:"novo-horizonte-baseline-2026-09-21",exercicios:[],despesas:[],receitas:[],apiUpdates:{},autoCheck:{}};
+window.TCESP_PERMANENT_BASELINE={manifest:"baseline-manifest.json",version:"novo-horizonte-baseline-2026-09-21"};
